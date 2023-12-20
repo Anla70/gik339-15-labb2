d8388990cb1a7f5790204ad3a3f63e8a1441a0b7
Anla70 <98649933+Anla70@users.noreply.github.com>
2023-12-20 10:10:20 +0100

    Update server

--- client/script.js
+++ client/script.js
@@ -1,28 +1,37 @@
+
 const url = "http://localhost:3000/users";
 fetch(url)
-    .then((response) => response.json())  
+    .then((response) => response.json())
     .then((users) => {
-        const userList = document.createElement("ul");
+        const grid = document.createElement("div");
+        grid.className = 'grid';
 
-        users.forEach((user) => {
-            const listItem = document.createElement("li");
-            listItem.style.backgroundColor = user.color;
-            listItem.id = `user-${user.id}`;
-            
+        users.forEach((user, index) => {
+            // Skapar en div för varje användare som kommer att innehålla både etiketten och användarens detaljer
+            const userDiv = document.createElement("div");
+            userDiv.className = 'grid-item';
 
-            listItem.innerHTML = `<p>Name: ${user.firstName} ${user.lastName}
-            <br>Username: ${user.username}</br></p>`;
-            console.log(user);
+            // Skapar en etikett för varje användare och nummer
+            const memberLabel = document.createElement("div");
+            memberLabel.className = 'member-label';
+            memberLabel.innerHTML = `Member<br>#${index + 1}`; // Sätter Member och #, vi har index som börjar på 0 och ökar med 1 för varje användare
 
-            userList.appendChild(listItem);
+            // Skapar en div för användarens namn och användarnamn
+            const userInfo = document.createElement("div");
+            userInfo.className = 'user-info';
+            // Sätter in användarens namn och användarnamn i userInfo
+            userInfo.innerHTML = `<p>${user.firstName} ${user.lastName}<br>Username: ${user.username}</p>`;
+            userInfo.style.backgroundColor = user.color; // Sätter bakgrundsfärgen på userInfo till användarens färg
+
+            // Skapar en etikett för varje användare 
+            userDiv.appendChild(memberLabel);
+            userDiv.appendChild(userInfo);
+
+            // Lägger till userDiv till i griden
+            grid.appendChild(userDiv);
         });
-
-        const container = document.querySelector('.container');
-        if (container) {
-            container.appendChild(userList);
-        } else {
-            console.error("Container element not found.");
-        }
+        // Hämtar container från index.html och lägger till grid där 
+        document.querySelector('.container').appendChild(grid); 
     })
     .catch((error) => {
         console.error("Error fetching users:", error);
--- server/server.js
+++ server/server.js
@@ -3,37 +3,49 @@ const server = express();
 const port = 3000;
 const sqlite3 = require("sqlite3").verbose();
 
-server
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // *?
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "*");
-    next(); // Skickar vidare till nästa 
-  });
+    next();
+});
 
-  // Skapar en databaskoppling
+// Skapar en databaskoppling
 const db = new sqlite3.Database("./gik339-labb2.db");
 
-// Skapar en GET-route för att hämta användardata från vår databas
+// Vi har en GET-route för att hämta användardata från databasen
 server.get("/users", (req, res) => {
-  // SQL-fråga med * för att hämta alla rader från tabellen users
-  const query = "SELECT * FROM users";
+    const query = "SELECT * FROM users";
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(err);
+        } else {
+            res.send(rows);
+        }
+    });
+});
 
-  // Använder instansen db.all() för att utföra SQL-frågan med en callback-funktion  (err, rows)
-  db.all(query, (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err); // Hanterar eventuella fel (500 = generellt server-fel)
-    } else {
-      res.send(rows); // Allt gick bra - skickar resultatet till klienten
-    }
+// Vi har en POST-route för att lägga till en ny användare
+server.post("/users", (req, res) => {
+    const { firstName, lastName, username, color } = req.body;
+    const query = `INSERT INTO users (firstName, lastName, username, color) VALUES (?, ?, ?, ?)`;
+    // Vi kör vår query 
+    db.run(query, [firstName, lastName, username, color], (err) => { 
+      if (err) {
+          console.error(err); // Skriver ut felmeddelandet i terminalen
+          res.status(500).send("!!!  Error adding user to the database  !!!"); // Visar i Postman att det inte gick att lägga till användaren
+      } else {
+          console.log("User added successfully!"); // Skriver ut i terminalen att användaren har lagts till
+          res.status(200).send("User added successfully!"); // Visar i Postman att användaren har lagts till
+      }
   });
 });
 
-// Startar servern medoch visar bekräftelsemeddelande
-server.listen(port, () => {console.log(`Server is running: ${port}`);});
+// Startar servern
+server.listen(port, () => {console.log(`Server is running on port: ${port}`);});
     
   
 
